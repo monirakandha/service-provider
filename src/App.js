e30ea18734205924026323e6813c51f5dc360057
monirhosenakanda <monirhosenakanda@gmail.com>
2022-04-18 13:12:38 +0600

    not found page added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register'
 import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
 import CheckOut from './Pages/CheckOut/CheckOut';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 
 
@@ -27,6 +28,7 @@ function App() {
           <Route path='/login' element={<Login></Login>}></Route>
           <Route path='/register' element={<Register></Register>}></Route>
           <Route path='/checkout' element={<RequireAuth> <CheckOut></CheckOut></RequireAuth>}></Route>
+          <Route path='*' element={<NotFound></NotFound>} ></Route>
         </Routes>
         <Footer></Footer>
     </div>
--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import NotFoundImg from '../../../images/404.png';
 const NotFound = () => {
     return (
         <div>
-            
+            <img src={NotFoundImg} alt="" srcset="" />
         </div>
     );
 };
